0e51124ff67fc498ccccb24473c80f4e8ba09ab5
wjonke <wesjonke@gmail.com>
2021-01-16 14:08:02 -0500

    added placeholder api fetch and framework for components

--- src/App.js
+++ src/App.js
@@ -1,33 +1,31 @@
-import { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+
+import { CardContainer } from './components/cardContainer/cardContainer';
 
 class App extends Component {
   constructor() {
     super();
+
     this.state = {
-      monsters: [],
+      users: [],
       searchField: '',
     };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((res) => res.json())
+      .then((users) => this.setState({ users: users }));
   }
 
   render() {
     return (
       <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className='App-link'
-            href='https://reactjs.org'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Learn React
-          </a>
-        </header>
+        <CardContainer />
+        {this.state.users.map((user) => (
+          <h1 key={user.id}>{user.name}</h1>
+        ))}
       </div>
     );
   }
---
+++ src/components/cardContainer/cardContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const CardContainer = (props) => {
+    return (
+        <div>
+            <h1>Hello from card container</h1>
+        </div>
+    )
+}
+
+
