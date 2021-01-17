ce2f38084d764097f470d57cb11f470aa99ab964
wjonke <wesjonke@gmail.com>
2021-01-17 16:07:53 -0500

    card container set to grid and mapping over users

--- src/App.js
+++ src/App.js
@@ -22,7 +22,11 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
-        <CardContainer users={this.state.users} />
+        <CardContainer>
+          {this.state.users.map((user) => (
+            <h1 key={user.id}>{user.name}</h1>
+          ))}
+        </CardContainer>
       </div>
     );
   }
--- src/components/cardContainer/cardContainer.jsx
+++ src/components/cardContainer/cardContainer.jsx
@@ -2,30 +2,13 @@ import React from 'react'
 import './cardContainer.css'
 
 export const CardContainer = (props) => {
-    console.log(props.users);
-    if (props.users.length > 1){
-        return (
-            <div 
-            className='.card-container' 
-            // style={{border:'1px solid black'}}
-            >
-
-                {props.users.map((user) => (
-                        <h1 key={user.id}>{user.name}</h1>
-                ))}
-
-            </div>
-        )
-
-    } else {
-        
-        return (
-            <div>
-     <h1>NO LIST</h1>
-            </div> 
-        ) 
-    }
-   
+    return (
+        props.children.length > 1 ? 
+            <div className='card-container'>{props.children}</div> : 
+            <div><h1>NO LIST TO SHOW</h1></div> 
+    )  
 }
+
+
 
 
